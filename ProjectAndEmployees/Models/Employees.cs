c2a8e42b5a510ec26448fd743373bfd8571ca090
kornelpost <post.kornel@gmail.com>
2021-03-05 10:51:30 +0100

    validation

--- ProjectAndEmployees/Models/Employees.cs
+++ ProjectAndEmployees/Models/Employees.cs
@@ -9,9 +9,17 @@ namespace ProjectAndEmployees.Models
     public class Employees
     {
         public int Id { get; set; }
+
         [Display(Name = "First Name")]
+        [RegularExpression(@"^[A-Z]+[a-z]*$")]
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
+        [RegularExpression(@"^[A-Z]+[a-z]*$")]
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public string LastName { get; set; }
 
         [Display(Name = "Join Date")]
