ff50b21a0ed895d7ad023eb2479800eac04ad8c9
kornelpost <post.kornel@gmail.com>
2021-03-05 09:45:03 +0100

    Employees model

--- ProjectAndEmployees/Models/Employees.cs
+++ ProjectAndEmployees/Models/Employees.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace ProjectAndEmployees.Models
 {
     public class Employees
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
     }
 }
