1a95ee30250519fc4257d3c4de401f73723d4501
kornelpost <post.kornel@gmail.com>
2021-03-05 10:11:17 +0100

    db context, connection string

--- ProjectAndEmployees/Startup.cs
+++ ProjectAndEmployees/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjectAndEmployees.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjectAndEmployees
 {
@@ -24,6 +26,8 @@ namespace ProjectAndEmployees
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ProjectAndEmployeesContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ProjectAndEmployeesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
